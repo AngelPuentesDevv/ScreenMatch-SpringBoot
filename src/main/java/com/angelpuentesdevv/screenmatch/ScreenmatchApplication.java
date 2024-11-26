c28c0af00b2c2b592bcfabf34abf96b051b732bd
Angel <apuentest@uniempresarial.edu.co>
2024-11-25 23:12:03 -0500

    primer commit

---
+++ src/main/java/com/angelpuentesdevv/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package com.angelpuentesdevv.screenmatch;
+
+import com.angelpuentesdevv.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?i=tt3896198&apikey=8e4ab839&t=death+note");
+		System.out.println(json);
+	}
+}
